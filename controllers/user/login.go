00ea4f53a4ce1022c4e5c165a633d09d00698854
橡皮擦 <2506152074@qq.com>
2023-05-10 21:08:39 +0800

    控制器

--- controllers/user/login.go
+++ controllers/user/login.go
@@ -1,1 +1,28 @@
-package user
+package users
+
+import (
+	"easy-gin/controllers"
+	receive "easy-gin/interaction/receive/user"
+	"easy-gin/service/user"
+	"github.com/gin-gonic/gin"
+)
+
+type LoginControllers struct {
+	controllers.BaseControllers
+}
+
+//Login 登入
+func (lg LoginControllers) Login(ctx *gin.Context) {
+	if rec, err := controllers.ShouldBind(ctx, new(receive.LoginReceive)); err == nil {
+		results, err := user.Login(rec)
+		lg.Response(ctx, results, err)
+	}
+}
+
+//Register 注册
+func (lg LoginControllers) Register(ctx *gin.Context) {
+	if rec, err := controllers.ShouldBind(ctx, new(receive.RegisterReceive)); err == nil {
+		results, err := user.Register(rec)
+		lg.Response(ctx, results, err)
+	}
+}
