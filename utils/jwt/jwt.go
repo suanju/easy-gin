90b38a6e3aa76e54feaa8caf8009465517ca1e5e
橡皮擦 <2506152074@qq.com>
2023-05-11 19:16:10 +0800

    工具类

---
+++ utils/jwt/jwt.go
@@ -0,0 +1,58 @@
+package jwt
+
+import (
+	"errors"
+	"fmt"
+	"github.com/dgrijalva/jwt-go"
+	"time"
+)
+
+// Hotkey 密钥
+var Hotkey = []byte("G0-store")
+
+// SaltStr  密码盐的随机字符串
+var SaltStr = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+//Claims  TOKEN 的结构体
+type Claims struct {
+	UserID uint
+	jwt.StandardClaims
+}
+
+// NextToken 登录以后签发jwt
+func NextToken(uid uint) string {
+	expireTime := time.Now().Add(7 * 24 * time.Hour)
+	claims := &Claims{
+		UserID: uid,
+		StandardClaims: jwt.StandardClaims{
+			//过期时间
+			ExpiresAt: expireTime.Unix(),
+			IssuedAt:  time.Now().Unix(),
+			// 签名颁发者
+			Issuer: "root",
+			//签名主题
+			Subject: "user token",
+		},
+	}
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	tokenString, err := token.SignedString(Hotkey)
+	if err != nil {
+		fmt.Println(err)
+	}
+	return tokenString
+}
+
+// ParseToken 解析 Token
+func ParseToken(tokenStr string) (*Claims, error) {
+	token, err := jwt.ParseWithClaims(tokenStr, &Claims{}, func(token *jwt.Token) (interface{}, error) {
+		return Hotkey, nil
+	})
+	if err != nil {
+		fmt.Println(" token parse err:", err)
+		return nil, err
+	}
+	if claims, ok := token.Claims.(*Claims); ok && token.Valid {
+		return claims, nil
+	}
+	return nil, errors.New("invalid token")
+}
