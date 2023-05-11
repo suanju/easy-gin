6c9ecc5f1e439e105b5b9fc3e28be1dd5ab0194a
橡皮擦 <2506152074@qq.com>
2023-05-11 19:15:02 +0800

    定义常量

---
+++ consts/redis.go
@@ -0,0 +1,11 @@
+package consts
+
+const (
+	/*
+		RegEmailVerCode	注册验证码
+		RegEmailVerCodeByForget 找回密码验证码
+	*/
+	RegEmailVerCode                       = "regEmailVerCode"
+	RegEmailVerCodeByForget               = "regEmailVerCodeByForget"
+	EmailVerificationCodeByChangePassword = "emailVerificationCodeByChangePassword"
+)
