f02c09025c4d9c4087b9989f37a84366fac4e044
橡皮擦 <2506152074@qq.com>
2023-05-11 19:15:53 +0800

    路由

--- router/user/login.go
+++ router/user/login.go
@@ -1,6 +1,9 @@
 package user
 
-import "github.com/gin-gonic/gin"
+import (
+	"easy-gin/controllers/user"
+	"github.com/gin-gonic/gin"
+)
 
 type LoginRouter struct {
 }
@@ -8,7 +11,7 @@ type LoginRouter struct {
 func (s *LoginRouter) InitLoginRouter(Router *gin.RouterGroup) {
 	loginRouter := Router.Group("login").Use()
 	{
-		loginControllers := new(users.LoginControllers)
+		loginControllers := new(user.LoginControllers)
 		loginRouter.POST("/register", loginControllers.Register)
 		loginRouter.POST("/login", loginControllers.Login)
 	}
