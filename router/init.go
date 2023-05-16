9c5538426d50b25d913e5e915fc83c4e0b38d5aa
橡皮擦 <2506152074@qq.com>
2023-05-16 22:11:37 +0800

    路由

--- router/init.go
+++ router/init.go
@@ -1,31 +1,35 @@
 package router
 
 import (
+	"easy-gin/router/user"
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
+	"log"
+	"path/filepath"
 )
 
 // RoutersGroup 路由总集
 type RoutersGroup struct {
+	user user.RouterGroup
 }
 
 // RoutersGroupApp 实例化
 var RoutersGroupApp = new(RoutersGroup)
 
-func InitRouter() {
+func init() {
 	router := gin.Default()
-	// 添加 cors 中间件
-	config := cors.DefaultConfig()
-	router.Use(cors.New(config))
+	//采用cors默认跨域中间件
+	//详细配置  https://github.com/gin-contrib/cors
+	router.Use(cors.Default())
 	PrivateGroup := router.Group("")
 	PrivateGroup.Use()
 	{
-		//规定您的静态资源目录
-		router.Static("/assets", "./assets")
+		router.Static("/assets", filepath.ToSlash("./assets")) //如果您的项目中需要展示静态资源
+		RoutersGroupApp.user.LoginRouter.InitLoginRouter(PrivateGroup)
 	}
 
 	err := router.Run()
 	if err != nil {
-		return
+		log.Fatal("路由加载失败")
 	}
 }
