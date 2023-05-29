a0b6d58a4c2c63988bd4238c8dd3f5a22339c668
橡皮擦 <2506152074@qq.com>
2023-05-29 23:30:07 +0800

    接口相关

--- interaction/receive/user/login.go
+++ interaction/receive/user/login.go
@@ -2,10 +2,9 @@ package user
 
 // RegisterReceive 用户注册
 type RegisterReceive struct {
-	UserName         string `json:"username" binding:"required"`
-	Password         string `json:"password" binding:"required"`
-	Email            string `json:"email" binding:"required,email"`
-	VerificationCode string `json:"verificationCode" binding:"required"`
+	UserName string `json:"username" binding:"required"`
+	Password string `json:"password" binding:"required"`
+	Email    string `json:"email" binding:"required,email"`
 }
 
 //LoginReceive 用户登入
